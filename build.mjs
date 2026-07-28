d693f88139d5f3ddc21fa2b11c427d35c705b3f2
Tiffany Lee <tjlee527@gmail.com>
2026-07-28 11:12:36 -0700

    Week 2 Storefront activites

--- build.mjs
+++ build.mjs
@@ -12,6 +12,16 @@ overrideGQLOperations([
     skipFragments: ['DOWNLOADABLE_ORDER_ITEMS_FRAGMENT'],
     operations: [],
   },
+  {
+    npm: '@dropins/storefront-pdp',
+    operations: [
+      `
+      fragment PRODUCT_FRAGMENT on ProductView {
+        metaTitle
+      }
+      `,
+    ],
+  },
   // {
   //   npm: '@dropins/storefront-checkout',
   //   operations: [],
